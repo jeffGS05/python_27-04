f076bb2f00205be19b8927c740bc7a3c2791c931
jeffGS05 <49353053+jeffGS05@users.noreply.github.com>
2019-04-27 09:46:00 -0600

    Add files via upload

---
+++ Practica1_27_04.py
@@ -0,0 +1,34 @@
+# Clase 3 listas
+
+mis_frutas=['fresa', 'manzana','mango', 'pera','naranja','piña']
+# this word "pass" mean don´t do anything at this point
+pass
+verduras=['tomates', 'papas','cebolals', 'ajos']
+frutas=['piña', 'naraja', 'sandia']
+carne=['mortadela', 'pollo', 'costilla de cerdo']
+limpieza=['jabon','cloro','shampoo']
+
+lista_del_compras=[verduras,frutas,carne,limpieza]
+print(lista_del_compras)
+
+# Paso 3 cuantos articulos hay en la lista
+print(len(lista_del_compras))
+
+# if return is not present the output will be null
+#Solucion 1
+cantidad=0
+for categoria in lista_del_compras:
+    print(categoria, len(categoria))
+    cantidad+=len(categoria)
+print(cantidad)
+
+#Solucion 2
+
+mi_lista=[]
+for articulos in lista_del_compras:
+    mi_lista.extend(articulos)
+    print(mi_lista)
+print(len(mi_lista))
+
+
+
